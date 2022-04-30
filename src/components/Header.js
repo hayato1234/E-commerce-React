a208e5d9cf5c187322f08a0b2fc364b6e2bb678c
Steve Moritani <steve.hayato.m@gmail.com>
2022-04-29 21:26:51 -0700

    lang dropdown added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,43 @@
 import React, { Component } from "react";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
 
 export default class Header extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      isLoginModalOpen: false,
+      isDropdownOpen: false
+    }
+    this.toggleLoginModal = this.toggleLoginModal.bind(this);
+    this.toggleLanguage = this.toggleLanguage.bind(this);
+  }
+
+  toggleLoginModal() {
+    this.setState({
+      isLoginModalOpen: !this.state.isLoginModalOpen
+    });
+  }
+  toggleLanguage(){
+    this.setState(prevState => ({
+      isDropdownOpen: !prevState.isDropdownOpen
+    }));
+  }
+
   render() {
     return (
       <React.Fragment>
@@ -18,38 +55,103 @@ export default class Header extends Component {
                 <h1>Rebuild Electronics</h1>
               </div>
               <div className="col-12 col-md-4 dropdown mt-2">
-                <a
+                <Button
                   className="btn btn-sm btn-primary"
                   role="button"
                   id="loginButton"
+                  color="primary"
+                  onClick={this.toggleLoginModal}
                 >
                   <i className="fa fa-user" aria-hidden="true"></i> Sign in
-                </a>
-                <button
-                  className="btn btn-sm btn-secondary dropdown-toggle mx-2"
-                  type="button"
-                  id="dropdownMenuButton"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  <i className="fa fa-globe" aria-hidden="true" /> Language
-                </button>
-                <div
-                  className="dropdown-menu"
-                  aria-labelledby="dropdownMenuButton"
-                >
-                  <a className="dropdown-item active" href="#">
-                    English
-                  </a>
-                  <a className="dropdown-item" href="/jp/index.html" lang="jp">
-                    日本語
-                  </a>
-                </div>
+                </Button>
+                <Dropdown isOpen={this.state.isDropdownOpen} toggle={this.toggleLanguage}>
+                  <DropdownToggle caret size="sm">
+                    <i className="fa fa-globe" aria-hidden="true" /> Language
+                  </DropdownToggle>
+                  <DropdownMenu>
+                    <DropdownItem>English</DropdownItem>
+                    <DropdownItem>日本語</DropdownItem>
+                  </DropdownMenu>
+                </Dropdown>
               </div>
             </div>
           </div>
         </header>
+
+        {/* <!-- login modal  --> */}
+        <Modal
+          isOpen={this.state.isLoginModalOpen}
+          toggle={this.toggleLoginModal}
+        >
+          <ModalHeader toggle={this.toggleLoginModal}>Login</ModalHeader>
+          <ModalBody>
+            <Form>
+              <FormGroup>
+                <Label class="sr-only col-form-label" htmlFor="loginEmail">
+                  Email Address
+                </Label>
+                <Input
+                  type="email"
+                  name="loginEmail"
+                  id="loginEmail"
+                  class="form-control form-control-sm"
+                  placeholder="Enter Email"
+                  required
+                />
+              </FormGroup>
+              <FormGroup class="form-row">
+                <div class="form-group col-12">
+                  <label class="sr-only col-form-label" for="loginPassward">
+                    Password
+                  </label>
+                  <Input
+                    type="password"
+                    name="password"
+                    id="loginPassword"
+                    class="form-control form-control-sm"
+                    placeholder="Password"
+                    required
+                  />
+                </div>
+              </FormGroup>
+              <FormGroup class="form-check">
+                <label class="form-check-label">
+                  <Input
+                    type="checkbox"
+                    class="form-check-input"
+                    name=""
+                    id=""
+                    value="checkedValue"
+                  />
+                  Remember me
+                </label>
+              </FormGroup>
+              <FormGroup class="form-row">
+                <Button
+                  type="button"
+                  class="btn btn-secondary btn-sm ml-auto"
+                  onClick={this.toggleLoginModal}
+                >
+                  Cancel
+                </Button>
+                <Button
+                  type="submit"
+                  id="loginBtn"
+                  class="btn btn-primary btn-sm ml-1"
+                  color="primary"
+                  disabled
+                >
+                  Sign in
+                </Button>
+              </FormGroup>
+            </Form>
+          </ModalBody>
+          <ModalFooter class="modal-footer">
+            <a class="btn btn-link btn-sm" href="#" data-dismiss="modal">
+              forgot your password?
+            </a>
+          </ModalFooter>
+        </Modal>
       </React.Fragment>
     );
   }
--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,90 +1,90 @@
 import React from "react";
 
-export default function ItemDetail(props) {
-    if(props.item){
-        console.log(props.item);
-        return (
-            <RenderItem item={props.item} />
-            // <div />
-        );
-    }else{
-        console.log("false");
-        return(
-            <div />
-        );
+export default class ItemDetail extends React.Component {
+
+  render() {
+    if (this.props.item) {
+      return (
+        <RenderItem item={this.props.item} />
+        // <div />
+      );
+    } else {
+      console.log("false");
+      return <div />;
     }
+  }
 }
 
-function RenderItem({item}) {
-    return (
-      <div className="container">
-        {/* <!-- breadcrumb row  --> */}
-        <div className="row">
-          <div className="col">
-            <nav className="breadcrumb">
-              <a className="breadcrumb-item" href="/home">
-                Home
-              </a>
-              <a id="breadc-category" className="breadcrumb-item" href="/home">
-                Camera and Photo
-              </a>
-              <span id="breadc-title" className="breadcrumb-item active">
-                Nikon D-22
-              </span>
-            </nav>
-          </div>
+function RenderItem({ item }) {
+  return (
+    <div className="container">
+      {/* <!-- breadcrumb row  --> */}
+      <div className="row">
+        <div className="col">
+          <nav className="breadcrumb">
+            <a className="breadcrumb-item" href="/home">
+              Home
+            </a>
+            <a id="breadc-category" className="breadcrumb-item" href="/home">
+              Camera and Photo
+            </a>
+            <span id="breadc-title" className="breadcrumb-item active">
+              Nikon D-22
+            </span>
+          </nav>
         </div>
+      </div>
 
-        {/* <!-- search bar  --> */}
-        <div className="form-row align-items-center mt-4">
-          <div className="col-6 mx-auto">
-            <div className="form-inline">
-              <div className="input-group">
-                <input
-                  className="form-control"
-                  type="text"
-                  placeholder="Search for items..."
-                />
-                <button className="btn btn-sm btn-secondary" type="submit">
-                  <i className="fa fa-search" aria-hidden="true"></i>
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <hr />
-
-        {/* <!-- item info  --> */}
-        <div className="row mt-5">
-          <div className="col">
-            <div className="media">
-              <img
-                id="detail-main-img"
-                src={"../"+item.imgPath}
-                alt="item"
-                className="d-flex img-fluid w-50"
+      {/* <!-- search bar  --> */}
+      <div className="form-row align-items-center mt-4">
+        <div className="col-6 mx-auto">
+          <div className="form-inline">
+            <div className="input-group">
+              <input
+                className="form-control"
+                type="text"
+                placeholder="Search for items..."
               />
-              <div className="media-body ml-5">
-                <h1 id="title">{item.title}</h1>
-                <h2 id="price">$ {item.price}</h2>
-                <button
-                  id="item-detail-add-cart"
-                  className="btn btn-warning my-4"
-                >
-                  <i className="fa fa-shopping-cart"></i>
-                  <span id="item-detail-add-cart-text">Add to Cart</span>
-                </button>
-                <hr />
-                <p id="item-detail-brand-text">Brand: {item.brand}</p>
-                <p id="item-detail-condition-text">Condition: {item.condition}</p>
-                <p id="item-detail-model-text">Model: {item.model}</p>
-                <p id="item-detail-year-text">Year: {item.year}</p>
-                <p id="item-detail-comment-text">Comment: {item.comment}</p>
-              </div>
+              <button className="btn btn-sm btn-secondary" type="submit">
+                <i className="fa fa-search" aria-hidden="true"></i>
+              </button>
             </div>
           </div>
         </div>
       </div>
-    );
+
+      <hr />
+
+      {/* <!-- item info  --> */}
+      <div className="row mt-5">
+        <div className="col">
+          <div className="media">
+            <img
+              id="detail-main-img"
+              src={"../" + item.imgPath}
+              alt="item"
+              className="d-flex img-fluid w-50"
+            />
+            <div className="media-body ml-5">
+              <h1 id="title">{item.title}</h1>
+              <h2 id="price">$ {item.price}</h2>
+              <button
+                id="item-detail-add-cart"
+                className="btn btn-warning my-4"
+              >
+                <i className="fa fa-shopping-cart"></i>
+                <span id="item-detail-add-cart-text">Add to Cart</span>
+              </button>
+              <hr />
+              <p id="item-detail-brand-text">Brand: {item.brand}</p>
+              <p id="item-detail-condition-text">Condition: {item.condition}</p>
+              <p id="item-detail-model-text">Model: {item.model}</p>
+              <p id="item-detail-year-text">Year: {item.year}</p>
+              <p id="item-detail-comment-text">Comment: {item.comment}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
